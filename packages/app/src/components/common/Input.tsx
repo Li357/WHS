ed3243930715897b64bf4b11c2da73f703416726
Andrew Li <andrewli357@gmail.com>
2019-07-28 00:47:30 -0500

    Add schedule removal

--- packages/app/src/components/common/Input.tsx
+++ packages/app/src/components/common/Input.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components/native';
 
 import {
   SUBTEXT_SIZE, SUBTEXT_FONT,
   FORM_HEIGHT, FORM_BORDER_RADIUS, FORM_PADDING_HORIZONTAL, INPUT_BORDER_WIDTH, FORM_MARGIN_VERTICAL,
 } from '../../constants/style';
-
-interface InputProps {
-  error?: boolean;
-}
+import { AppState } from '../../types/store';
 
 const StyledInput = styled.TextInput<InputProps>`
   color: ${({ theme }) => theme.textColor};
@@ -25,6 +23,11 @@ const StyledInput = styled.TextInput<InputProps>`
   margin: ${FORM_MARGIN_VERTICAL} 0;
 `;
 
+interface InputProps {
+  error?: boolean;
+}
+
 export default function Input(props: InputProps & TextInputProps) {
-  return (<StyledInput autoCorrect={false} autoCapitalize="none" {...props} />);
+  const theme = useSelector((state: AppState) => state.theme);
+  return (<StyledInput selectionColor={theme.accentColor} autoCorrect={false} autoCapitalize="none" {...props} />);
 }
--- packages/app/src/screens/AddSchedule.tsx
+++ packages/app/src/screens/AddSchedule.tsx
@@ -2,30 +2,51 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import fetch from 'react-native-fetch-polyfill';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import authorizedRoute from '../components/common/authorizedRoute';
 import Input from '../components/common/Input';
+import Subtext from '../components/common/Subtext';
 import { TEACHER_URL, FETCH_TIMEOUT, SEARCH_URL } from '../constants/fetch';
 import { RawTeacherData } from '../types/schedule';
 import { reportError, notify } from '../utils/utils';
 import TeacherItem from '../components/add-schedule/TeacherItem';
-import { FORM_BORDER_RADIUS, FORM_MARGIN_VERTICAL } from '../constants/style';
+import CurrentTeacherItem from '../components/add-schedule/CurrentTeacherItem';
+import { FORM_BORDER_RADIUS, FORM_MARGIN_VERTICAL, SMALLTEXT_SIZE, FORM_PADDING_HORIZONTAL } from '../constants/style';
 import { AppState } from '../types/store';
-import { addTeacherSchedule } from '../actions/creators';
+import { addTeacherSchedule, setTeacherSchedules } from '../actions/creators';
 import { getUserScheduleFromHTML, parseHTMLFromURL } from '../utils/process-info';
+import { ScrollView } from 'react-native';
 
-const TeacherContainer = styled.View`
+const ListContainer = styled.View`
   border-radius: ${FORM_BORDER_RADIUS};
   background-color: ${({ theme }) => theme.foregroundColor};
   margin: ${FORM_MARGIN_VERTICAL} 0;
 `;
 
+const InfoContainer = styled.View`
+  height: 50%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InfoText = styled(Subtext)`
+  text-align: center;
+`;
+
+const Title = styled(Subtext)`
+  margin-left: ${FORM_PADDING_HORIZONTAL};
+  font-size: ${SMALLTEXT_SIZE};
+`;
+
 export default authorizedRoute('Add Schedule', function AddSchedule() {
   const teacherSchedules = useSelector(({ user }: AppState) => user.teacherSchedules);
+  const theme = useSelector((state: AppState) => state.theme);
   const dispatch = useDispatch();
   const [query, setQuery] = useState('');
   const [teachers, setTeachers] = useState<RawTeacherData[]>([]);
   const [addingTeacher, setAddingTeacher] = useState(false);
+  const [removingTeacher, setRemovingTeacher] = useState(false);
   const controller = new AbortController();
   const signal = controller.signal;
 
@@ -67,6 +88,14 @@ export default authorizedRoute('Add Schedule', function AddSchedule() {
     setAddingTeacher(false);
   };
 
+  const removeSchedule = (name: string) => () => {
+    setRemovingTeacher(true);
+    const removed = teacherSchedules.filter((teacher) => teacher.name !== name);
+    dispatch(setTeacherSchedules(removed));
+    notify('Success', `${name}'s schedule removed.`);
+    setRemovingTeacher(false);
+  };
+
   // useEffect cannot have async handlers and React Suspense isn't ready yet
   useEffect(() => {
     if (query.length === 0) {
@@ -80,12 +109,38 @@ export default authorizedRoute('Add Schedule', function AddSchedule() {
   const teacherItems = teachers.map((teacher) => (
     <TeacherItem key={teacher.id} teacher={teacher} onPress={addSchedule(teacher)} disabled={addingTeacher} />
   ));
+
+  const currentTeachers = teacherSchedules.map(({ name }) => (
+    <CurrentTeacherItem key={name} name={name} onPress={removeSchedule(name)} disabled={removingTeacher} />
+  ));
+
+  const currentTeachersInfo = (
+    <>
+      <Title>Current teachers' schedules</Title>
+      <ListContainer>{currentTeachers}</ListContainer>
+    </>
+  );
+
+  const info = (
+    <InfoContainer>
+      <Icon name="add" size={100} color={theme.subtextColor} />
+      <InfoText numberOfLines={3}>
+        Add a teacher's schedule. Once added, they will show up under your schedule.
+      </InfoText>
+    </InfoContainer>
+  );
+
+  const lists = (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <ListContainer>{teacherItems}</ListContainer>
+      {teacherSchedules.length > 0 && currentTeachersInfo}
+    </ScrollView>
+  );
+
   return (
     <>
       <Input placeholder="Teacher Name" value={query} onChangeText={setQuery} />
-      <TeacherContainer>
-        {teacherItems}
-      </TeacherContainer>
+      {query.length === 0 && teacherSchedules.length === 0 ? info : lists}
     </>
   );
 });
