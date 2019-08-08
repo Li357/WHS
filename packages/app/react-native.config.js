4ed2c9b5e50217b1d2b7cbe62872b02fd942846c
Andrew Li <andrewli357@gmail.com>
2019-08-07 22:48:55 -0500

    Full migration to RN 0.60 with autolinking

--- packages/app/react-native.config.js
+++ packages/app/react-native.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  assets: ['./assets'],
+  assets: ['./assets', 'react-native-vector-icons'],
+  dependencies: {
+    'react-native-code-push': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'bugsnag-react-native': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'react-native-background-fetch': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
 };
