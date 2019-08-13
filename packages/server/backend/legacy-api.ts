c342885c718e3bdacf167ce2856768edb9116168
Andrew Li <andrewli357@gmail.com>
2019-08-13 17:42:30 -0500

    Adapt old legacy API to dates collection

--- packages/server/backend/legacy-api.ts
+++ packages/server/backend/legacy-api.ts
@@ -2,18 +2,11 @@ import { Router } from 'express';
 import { format } from 'date-fns';
 
 import { asyncRoute } from './utils';
-import { DateTypeKey, LegacyDate, LegacyDateSchema, LegacySettingsSchema, Settings } from './types/legacy-api';
+import { DateSchema } from '../shared/types/api';
 
 // Beware of spaghetti
 
 const legacyRouter = Router();
-
-const dateTypeKeys = {
-  1: 'assemblyDates',
-  2: 'noSchoolDates',
-  3: 'lateStartDates',
-  4: 'earlyDismissalDates',
-};
 
 // < v2.0-b6 SUPPORT
 legacyRouter.get('/otherDates', asyncRoute(async ({ db }, res, next) => {
@@ -24,23 +17,20 @@ legacyRouter.get('/otherDates', asyncRoute(async ({ db }, res, next) => {
 
 // < v2.0-b6 SUPPORT
 legacyRouter.get('/specialDates', asyncRoute(async ({ db }, res, next) => {
-  const currentYear = new Date().getFullYear();
-  const [asm, nos, lts, ead] = (await Promise.all(Array(4).fill(undefined).map((item, i) => (
-    db!.collection('specialDates').findOne({
-      type: String(i + 1),
-      year: String(currentYear),
-    })
-  )))).map((doc) => (doc ? doc.dates : []).map((dateObj: LegacyDate) => (
+  const currentYear = String(new Date().getFullYear());
+  const [asm, nos, lts, ead] = (await Promise.all([
+    'assembly', 'no-school', 'late-start', 'early-dismissal',
+  ].map((type) => (
+    db!.collection('dates').find({ year: currentYear, type }).toArray()
+  )))).map((arr) => arr.map((dateObj: DateSchema) => (
     format(new Date(dateObj.date), 'MMMM D YYYY')
   )));
 
-  const { settings } = await db!.collection<LegacyDateSchema | LegacySettingsSchema>('specialDates').findOne({
-    type: '5',
-    year: String(currentYear),
-  }) as LegacySettingsSchema;
-  (Object.keys(settings) as Array<keyof Settings>).forEach((key) => {
-    settings[key] = format(new Date(settings[key]), 'MMMM D YYYY');
-  });
+  const [sos, soe, sts, ste] = (await Promise.all([
+    'semester-one-start', 'semester-one-end', 'semester-two-start', 'semester-two-end',
+  ].map((type) => (
+    db!.collection('dates').find({ year: currentYear, type }).toArray()
+  )))).map((arr) => format(new Date(arr[0].date), 'MMMM D YYYY'));
 
   res.status(200).json({
     assemblyDates: asm,
@@ -48,40 +38,54 @@ legacyRouter.get('/specialDates', asyncRoute(async ({ db }, res, next) => {
     noSchoolDates: nos,
     lateStartDates: lts,
     earlyDismissalDates: ead,
-    ...settings,
+    semesterOneStart: sos,
+    semesterOneEnd: soe,
+    semesterTwoStart: sts,
+    lastDay: ste,
   });
 }));
 
-// 2.x SUPPORT
 legacyRouter.get('/api/specialDates', asyncRoute(async (
   { db, query: { type: dateType, year, onlyDates } }, res, next,
 ) => {
   if (!dateType && year) {
-    const docs = await db!.collection('specialDates').find({ year }).toArray();
-
-    // Request for only dates by app
     if (onlyDates) {
-      const specialDates = docs.reduce((datesDict, { type, dates, settings }) => {
-        if (type === '5') {
-          return {
-            ...datesDict,
-            ...settings,
-          };
-        }
+      const dateArrays = await Promise.all(['assembly', 'no-school', 'late-start', 'early-dismissal'].map((type) => (
+        db!.collection('dates').find({ year, type }).toArray()
+      )));
+      const [
+        assemblyDates,
+        noSchoolDates,
+        lateStartDates,
+        earlyDismissalDates,
+      ] = dateArrays.map((dateArray) => dateArray.map((dateObj) => new Date(dateObj.date).toISOString()));
 
-        // eslint-disable-next-line no-param-reassign
-        datesDict[dateTypeKeys[type as DateTypeKey]] = [
-          ...datesDict[dateTypeKeys[type as DateTypeKey]] || [],
-          ...dates.map((obj: LegacyDate) => obj.date),
-        ];
-        return datesDict;
-      }, {});
-      res.status(200).json(specialDates);
-      return;
+      const settings = await Promise.all([
+        'semester-one-start', 'semester-one-end', 'semester-two-start', 'semester-two-end',
+      ].map((type) => (
+        db!.collection('dates').find({ year, type }).toArray()
+      )));
+      const [
+        semesterOneStart,
+        semesterOneEnd,
+        semesterTwoStart,
+        lastDay,
+      ] = settings.map((dateArray) => new Date(dateArray[0].date).toISOString());
+
+      return res.status(200).json({
+        assemblyDates,
+        noSchoolDates,
+        lateStartDates,
+        earlyDismissalDates,
+        semesterOneStart,
+        semesterOneEnd,
+        semesterTwoStart,
+        lastDay,
+      });
     }
 
-    res.status(200).json(docs);
-    return;
+    const docs = await db!.collection('dates').find({ year }).toArray();
+    return res.status(200).json(docs);
   }
 
   const doc = await db!.collection('specialDates').findOne({ type: dateType, year });
